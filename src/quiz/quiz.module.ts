19240a1c3a5c61d03a06eb7df42f8f14900eb279
Nayden <soodker@gmail.com>
2018-12-18 21:59:24 +0200

    all endpoints created

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -1,15 +1,15 @@
-import { ExamController } from './exam.controller';
 import { AuthModule } from '../auth/auth.module';
 import { AuthService } from '../auth/auth.service';
 import { CoreModule } from '../common/core/core.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { User } from '../data/entities/user.entity';
+import { UsersController } from './exam.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), CoreModule, AuthModule],
   providers: [],
   exports: [],
-  controllers: [ExamController],
+  controllers: [QuizController],
 })
-export class UsersModule { }
+export class UsersModule {}
